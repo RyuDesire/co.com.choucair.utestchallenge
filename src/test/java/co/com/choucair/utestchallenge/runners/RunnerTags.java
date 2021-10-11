a62a4dc951dedc3589a8798381fe81280fc874c7
Ryu Desire <cesarstiven2017@gmail.com>
2021-10-11 16:54:45 -0500

    [utestchallenge] c002 - senerinty.properties, build.gradle configured and chrome driver added

---
+++ src/test/java/co/com/choucair/utestchallenge/runners/RunnerTags.java
@@ -0,0 +1,17 @@
+package co.com.choucair.utestchallenge.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resourses/demo.feature",
+        tags = "@CreateUser",
+        glue="co.com.choucair.utestchallenge.stepdefinitions",
+        snippets = SnippetType.CAMELCASE
+)
+
+public class RunnerTags {
+}
